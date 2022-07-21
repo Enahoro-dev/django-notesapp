67789d738de3c01f247105a62244eb09110b9ed8
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-07-21 10:28:56 +0100

    static update

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS =[
     BASE_DIR/ 'front/build/static'
 ]
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'front/build/static/')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
