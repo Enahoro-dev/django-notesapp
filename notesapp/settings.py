a6cca4f82e2058eba1b3bd53d636c700d9d5c80a
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-07-21 09:53:40 +0100

    adding react frontend

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR/'frontend/build'
+            BASE_DIR/'front/build'
         
         ],
         'APP_DIRS': True,
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS =[
-    BASE_DIR/ 'frontend/build/static'
+    BASE_DIR/ 'front/build/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
